55a44e75dfe119c782631ba81368d58ca7589fac
Simon Bang <bangtsimon@gmail.com>
2016-12-07 02:06:16 -0800

    completed the menu + styling

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,55 +1,78 @@
 import React from "react";
 import {Button, Card, CardText, Cell, Grid, CardTitle} from 'react-mdl';
-
+console.log(TERIYAKI);
 export default class extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {current: ""}
-    }
-
-    /* MAKE THIS WORK!!!!!!*/
-    getMenuItems(type) {
-        fetch("./data/yb-menu.js")
-            .then(response => response.json())
-            .then(data => this.setState({
-                current: type,
-                menu: data
-            }));
-    }
-
-    componentdidMount() {
-        fetch("./data/yb-menu.js")
-            .then(response => response.json())
-            .then(data => this.setState({
-                current: Teriyaki,
-                menu: data
-            }));
+        this.state = {teriyaki: TERIYAKI, korean: KOREAN, chinese: CHINESE}
     }
 
     render() {
-        var menuItems;
+        var teriyakiItems;
+        var koreanItems;
+        var chineseItems;
         if (this.state.teriyaki) {
-            menuItems = this.state.teriyaki.items.map(item => 
+            teriyakiItems = this.state.teriyaki.map(item => 
                 <Cell key = {item.id} col={6} style={{margin:'7.5px'}}>
                     <Card className="card" shadow={0} style={{width: '100%', height: '100px', margin: '10px', position: "relative"}}>
                         <div className="itemIMG">
-                            <img src={item.imgURL} style={{left: "0", position: "absolute", height: "100%", width: "200px"}}/>
+                            <img className="item-pic" src={item.imgURL} style={{left: "0", position: "absolute", height: "100%"}}/>
                         </div>
-                        <CardTitle style={{marginLeft: "200px", textAlign: "center"}}>{item.name}</CardTitle>
-                        <CardText style={{marginLeft: "200px", paddingTop: "0px", flex: "auto", width: "auto"}}>
-                            <p>{item.description}</p>
-                            <p>Price: ${item.price}</p>
+                        <CardTitle className="card-title" style={{padding: "0px"}}>
+                            <h4 className="item-title">{item.name}</h4>
+                        </CardTitle>
+                        <CardText style={{flex: "auto", width: "auto"}}className="card-text">
+                            <p>{item.description}<br/><span>Price: ${item.price}</span></p>
+         
                         </CardText>
                     </Card> 
                 </Cell>
             );
+            koreanItems = this.state.korean.map(item => 
+                <Cell key = {item.id} col={6} style={{margin:'7.5px'}}>
+                    <Card className="card" shadow={0} style={{width: '100%', height: '100px', margin: '10px', position: "relative"}}>
+                        <div className="itemIMG">
+                            <img src={item.imgURL} style={{left: "0", position: "absolute", height: "100%"}}/>
+                        </div>
+                        <CardTitle style={{padding: "0px"}}className="card-title">
+                            <h4 className="item-title">{item.name}</h4>    
+                        </CardTitle>
+                        <CardText className="card-text" style={{flex: "auto", width: "auto"}}>
+                            <p>{item.description}<br/><span>Price: ${item.price}</span></p>
+                        </CardText>
+                    </Card> 
+                </Cell>
+            );    
+
+            chineseItems = this.state.chinese.map(item => 
+                <Cell key = {item.id} col={6} style={{margin:'7.5px'}}>
+                    <Card className="card" shadow={0} style={{width: '100%', height: '100px', margin: '10px', position: "relative"}}>
+                        <div className="itemIMG">
+                            <img src={item.imgURL} style={{left: "0", position: "absolute", height: "100%"}}/>
+                        </div>
+                        <CardTitle style={{padding: "0px"}}className="card-title">
+                            <h4 className="item-title">{item.name}</h4>
+                        </CardTitle>
+                        <CardText className="card-text" style={{flex: "auto", width: "auto"}}className="card-text">
+                            <p>{item.description}<br/><span>Price: ${item.price}</span></p>
+                        </CardText>
+                    </Card> 
+                </Cell>
+            );   
         }
-        console.log(menuItems);
         return (
             <div className="container">
-                <h1 style={{textAlign: "center"}}>{this.state.current} Menu</h1>
+                <h2 style={{textAlign: "center"}}>Teriyaki</h2>
                     <Grid className="grid">
-                        {menuItems}
+                        {teriyakiItems}
+                    </Grid>
+                <h2 style={{textAlign: "center"}}>Korean Food</h2>
+                    <Grid>
+                        {koreanItems}
+                    </Grid>
+                <h2 style={{textAlign: "center"}}>Chinese & Wok</h2>
+                    <Grid>
+                        {chineseItems}
                     </Grid>
             </div>
         );
