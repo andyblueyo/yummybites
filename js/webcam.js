c8a672a26052f8ffa7fc44a5b8a05c7af565a28f
Andrea Chen <abchen@uw.edu>
2016-12-08 03:11:50 -0800

    layout for camera

--- js/webcam.js
+++ js/webcam.js
@@ -3,10 +3,15 @@
 var video = document.querySelector("video");
 var canvas = document.getElementById("bg");
 var ctx = canvas.getContext("2d");
-var canvas2 = document.getElementById("fg");
-var ctx2 = canvas2.getContext("2d");
+// var canvas2 = document.getElementById("fg");
+// var ctx2 = canvas2.getContext("2d");
 var filterSel = document.querySelector("#filter-select");
 var image = document.getElementById("imgCanvas");
+
+var downloadButton = document.getElementById("btn-download");
+downloadButton.disabled = true;
+
+
 
 
 
@@ -25,12 +30,11 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             document.getElementById("snap").addEventListener("click", function() {
                 canvas.width = video.clientWidth;
                 canvas.height = video.clientHeight;
+                // erase the picture
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx2.clearRect(0, 0, canvas.width, canvas.height);
-
+                // create the picture
                 ctx.drawImage(video, 0, 0, video.clientWidth, video.clientHeight);
-                ctx2.drawImage(image, 100, 100);
-
+                ctx.drawImage(image, 50, 50);
             });
             
             filterSel.addEventListener("change", function() {
@@ -50,20 +54,20 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     alert("Sorry, your browser doesn't allow turning on the web cam, use Chrome or Firefox");
 }
 
-// ctx2.clearRect(0, 0, canvas.width, canvas.height);
 
-// to download an canvas as a png 
+
+// to download a canvas as a png 
 // http://weworkweplay.com/play/saving-html5-canvas-as-image/
 
-// mirror.addEventListener('contextmenu', function (e) {
-//     var dataURL = canvas.toDataURL('image/png');
-//     mirror.src = dataURL;
-// });
-// var button = document.getElementById('btn-download');
-// button.addEventListener('click', function (e) {
-//     var dataURL = canvas.toDataURL('image/png');
-//     button.href = dataURL;
-// });
+mirror.addEventListener('contextmenu', function (e) {
+    var dataURL = canvas.toDataURL('image/png');
+    mirror.src = dataURL;
+});
+
+downloadButton.addEventListener('click', function (e) {
+    var dataURL = canvas.toDataURL('image/png');
+    downloadButton.href = dataURL;
+});
 
 
 
